d2345ad6a888e96cda9add99eacaf66fc9e61e38
sagarjain3 <sagarjain31200@gmail.com>
2025-07-15 16:34:43 +0530

    Update Auth.js

--- backend/controller/Auth.js
+++ backend/controller/Auth.js
@@ -31,8 +31,8 @@ export const signUp = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
-            secure: false
+            sameSite: "None",
+            secure: true
         })
 
         return res.status(201).json(user)
@@ -70,8 +70,8 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
-            secure: false
+           sameSite: "None",
+            secure: true
         })
 
         return res.status(200).json(user)
@@ -87,8 +87,8 @@ export const logOut = async (req, res) => {
         // Clear cookie with matching options
         res.clearCookie("token", {
             httpOnly: true,
-            sameSite: "lax",
-            secure: false
+           sameSite: "None",
+            secure: true
         })
         return res.status(200).json({ msg: "logOut successful" })
 
